29d5036ca5efd501f4465f7141ef45cd96af6d29
eedo_y <doup2001@naver.com>
2024-10-23 20:05:22 +0900

    chore : 기본세팅

---
+++ src/main/java/org/gdg/zipte_gdg/ZipteGdgApplication.java
@@ -0,0 +1,13 @@
+package org.gdg.zipte_gdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipteGdgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipteGdgApplication.class, args);
+    }
+
+}
