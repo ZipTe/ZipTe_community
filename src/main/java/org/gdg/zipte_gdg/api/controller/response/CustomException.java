7ca11fbed32d0595f10b3e626101f94028ec32d5
eedo_y <doup2001@naver.com>
2024-10-25 02:10:49 +0900

    feat : 공통 ApiResponse 구현 게시글 참고 - https://velog.io/@tkdwns414/Spring-Boot-공통-응답용-ApiResponse-만들기

---
+++ src/main/java/org/gdg/zipte_gdg/api/controller/response/CustomException.java
@@ -0,0 +1,14 @@
+package org.gdg.zipte_gdg.api.controller.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public String getMessage() {
+        return errorCode.getMessage();
+    }
+}
