560289594b1e222c4d451c41424c0121f50d6721
eedo_y <doup2001@naver.com>
2024-10-24 20:35:00 +0900

    test : JPQL 로직 테스트 통과

--- src/main/java/org/gdg/zipte_gdg/domain/member/MemberRepository.java
+++ src/main/java/org/gdg/zipte_gdg/domain/member/MemberRepository.java
@@ -1,7 +1,18 @@
 package org.gdg.zipte_gdg.domain.member;
 
+import org.gdg.zipte_gdg.domain.review.Comment;
+import org.gdg.zipte_gdg.domain.review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select r from Member m join m.reviews r where m.id = :memberId")
+    List<Review> findReviewsByMemberId(@Param("memberId") Long memberId);
+
+    @Query("select r from Member m join m.comments r where m.id = :memberId")
+    List<Comment> findCommentsByMemberId(@Param("memberId") Long memberId);
 
 }
