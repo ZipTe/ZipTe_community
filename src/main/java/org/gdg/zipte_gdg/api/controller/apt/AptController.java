8ba39f15a922bf291e7ad3cc736601c4b4cadc85
eedo_y <doup2001@naver.com>
2024-12-12 17:12:17 +0900

    feat : fastapi와 연동 - AptController 설정 - AptServiceImpl 구현 - webflux바탕으로 WebClientConfig 설정

---
+++ src/main/java/org/gdg/zipte_gdg/api/controller/apt/AptController.java
@@ -0,0 +1,55 @@
+package org.gdg.zipte_gdg.api.controller.apt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.gdg.zipte_gdg.api.controller.response.ApiResponse;
+import org.gdg.zipte_gdg.api.service.apt.AptService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/apt")
+public class AptController {
+
+    private final AptService aptService;
+
+    @GetMapping("/AI")
+    public Mono<ApiResponse<Object>> getAl(
+            @RequestParam String apartment_name,
+            @RequestParam int count) {
+
+        final var response = aptService.getListByAI(apartment_name,count);
+        return response.flatMap(data -> Mono.just(ApiResponse.ok(data)));
+    }
+
+    @GetMapping()
+    public Mono<ApiResponse<Object>> getOne(@RequestParam String kaptCode){
+
+        final var response = aptService.getOne(kaptCode);
+        return response.flatMap(data -> Mono.just(ApiResponse.ok(data)));
+    }
+
+    @GetMapping("/price")
+    public Mono<ApiResponse<Object>> getPriceDong(@RequestParam String dong, @RequestParam int year){
+
+        final var response = aptService.getPriceByDong(dong,year);
+        return response.flatMap(data -> Mono.just(ApiResponse.ok(data)));
+    }
+
+    @GetMapping("/price/size")
+    public Mono<ApiResponse<Object>> getPriceDongSize(@RequestParam String dong, @RequestParam String size, @RequestParam int year){
+
+        final var response = aptService.getPriceByDongAndSize(dong,size,year);
+        return response.flatMap(data -> Mono.just(ApiResponse.ok(data)));
+    }
+
+    @GetMapping("/price/apt")
+    public Mono<ApiResponse<Object>> getPriceApt(@RequestParam String apt_name, @RequestParam String size, @RequestParam int year){
+
+        final var response = aptService.getPriceByApt(apt_name,size,year);
+        return response.flatMap(data -> Mono.just(ApiResponse.ok(data)));
+    }
+
+}
--- src/main/java/org/gdg/zipte_gdg/api/service/apt/AptService.java
+++ src/main/java/org/gdg/zipte_gdg/api/service/apt/AptService.java
@@ -2,6 +2,7 @@ package org.gdg.zipte_gdg.api.service.apt;
 
 import org.gdg.zipte_gdg.api.controller.apt.request.AptRequestDto;
 import org.gdg.zipte_gdg.api.service.apt.response.AptResposnseDto;
+import reactor.core.publisher.Mono;
 
 public interface AptService {
 
@@ -9,16 +10,24 @@ public interface AptService {
     AptResposnseDto register (AptRequestDto aptRequestDto);
 
     // 찾기
-    AptResposnseDto getOne(Long id);
+//    AptResposnseDto getOne(Long id);
+    Mono<Object> getOne(String kaptCode);
+    // 평점 높은 아파트 찾기
 
-    // 아파트 목록 찾기 <특정동의 아파트 찾기>
+    // 아파트 목록 찾기 <특정 동의 아파트 찾기>
 
-    // 아파트 목록 찾기 <특정지하철의 아파트 찾기>
+    // 아파트 목록 찾기 <특정 지하철의 아파트 찾기>
 
-    // 아파트 관련 리뷰 찾기
+    // AI 활용하여 비슷한 아파트 추천 가져오기 구현하기
+    Mono<Object> getListByAI(String apartment_name, int top_n);
 
-    // 평점 높은 리뷰 찾기
+    // 동에 따른 1년간 평당 가격 추이
+    Mono<Object> getPriceByDong(String dong, int year);
 
+    // 동, 평에 따른 평당 가격 추이
+    Mono<Object> getPriceByDongAndSize(String dong,String size, int year);
 
+    // 아파트 따른 평당 가격 추이
+    Mono<Object> getPriceByApt(String apt_name, String size, int year);
 
 }
